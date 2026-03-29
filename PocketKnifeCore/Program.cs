dbada41a8145a6c28ddca9ef2076e6ebeaab9c31
Hunter Dyar <bloop@hdyar.com>
2026-03-29 19:58:51 -0400

    start of program execution

--- PocketKnifeCore/Program.cs
+++ PocketKnifeCore/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace PocketKnifeCore;
+﻿using PocketKnifeCore.Engine;
+
+namespace PocketKnifeCore;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var p = Blender.GetBlenderPath();
-        Console.WriteLine(p);
+        if (args.Length == 0)
+        {
+            Console.WriteLine("PocketKnife (V0.0). Provide an input pocketknife script as the first argument. For any following arguments, the script will be run from the top with this starting string.");
+            return;
+        } 
+        FileInfo script = new FileInfo(args[0]);
+        ExecuteScript(script, args);
+    }
+
+    private static void ExecuteScript(FileInfo script, string[] arguments)
+    {
+        if (!script.Exists)
+        {
+            Console.Error.WriteLine($"Error: Cannot Find File {script.FullName}.");
+        }
+
+        PKString[] pkStrings = new PKString[arguments.Length - 1];
+        for (int i = 1; i < arguments.Length; i++)
+        {
+            pkStrings[i - 1] = new PKString(arguments[i]);
+        }
+
+        using var stream = script.OpenText();
+        var source = stream.ReadToEnd();
+        try
+        {
+            var p = new Parser.Parser();
+            p.Parse(source);
+            var compiler = new Compiler();
+            compiler.CompileScript(p.Program);
+            var i = new Interpreter();
+            i.Execute(compiler.Script, pkStrings);
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e);
+            throw;
+        }
     }
 }
--- PocketKnifeCore/Runtime/Interpreter.cs
+++ PocketKnifeCore/Runtime/Interpreter.cs
@@ -11,4 +11,19 @@ public class Interpreter
 		
 		//yep, after compilation, all we do is call 'execute' on the root, and it does the rest!
 	}
+
+	public void Execute(PocketKnifeScript script, PKString input)
+	{
+		var context = new Context(input);
+		script.Execute(context);
+	}
+
+	public void Execute(PocketKnifeScript script, PKString[] input)
+	{
+		for (int i = 0; i < input.Length; i++)
+		{
+			var context = new Context(input[i]);
+			script.Execute(context);
+		}
+	}
 }
