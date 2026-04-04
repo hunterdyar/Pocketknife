97e998c59a1b28fac4340a34502daaf32a3cc98b
Hunter Dyar <bloop@hdyar.com>
2026-04-04 19:07:25 -0400

    thinking about 'helpful, lenient path combine with reasonable assumptions', which is what my notes say. but now im looking at the reality of files/dirs and everything is chaos.

---
+++ PocketKnifeCore/Runtime/Features/PathAssembler.cs
@@ -0,0 +1,67 @@
+﻿namespace PocketKnifeCore.Engine;
+
+public class PathAssembler
+{
+	private string[] _pieces;
+	private PathSegment[] _segments;
+	public PathAssembler(params string[] pieces)
+	{
+		_pieces = pieces;
+		_segments = CreateSegments(pieces);
+	}
+
+	private PathSegment[] CreateSegments(string[] pieces)
+	{
+		return pieces.Select(x => new PathSegment(x)).ToArray();
+	}
+
+	private string Assemble()
+	{
+		//if we know that the file should exist, we can get better answers (dir or file, etc)
+			//with File.GetAttributes. we can declare for sure if it's a dir or file or so on.
+				//so we check that, whythefucknot.
+		
+		
+		//our goal is to provide reasonable assists to this function.
+			//insert trailing or preceding directory seperator characters if neccesary.
+				//it's assumed (what we know that Path.combine does not):
+					//that our pieces MUST be directory gaps except for the last 2
+					//the last 1 is a dir, or filename, or just extension.
+					//if las (could be just) extension, the second to last is a dir or filename
+						//.git (dir), .gitignore (file) bro nobody knows anything.
+		
+		return Path.Combine(_pieces);
+	}
+	
+	
+	
+	
+}
+
+public struct PathSegment
+{
+	public string Piece;
+	public bool HasExt;
+	public string Ext;
+	public string Filename;
+	public PathSegment(string piece)
+	{
+		if (piece.StartsWith("."))
+		{
+			if (Piece.Count(x => x == '.') == 1)
+			{
+				//this is a 'hidden directory' or an extension.
+				//wait, extensions can have multiple dots too. .tar.gz, .xaml.cs
+				//and so cna directories.
+				
+				
+				
+				//man, fuck computers.
+			}
+		}
+		Piece = Path.TrimEndingDirectorySeparator(piece);
+		HasExt = Path.HasExtension(Piece);
+		Ext = Path.GetExtension(Piece);
+		Filename = Path.GetFileNameWithoutExtension(Piece);
+	}
+}
